45b2b02418b2f42063784f7ddf0ff142ee28abaf
Fabrizzio Ontaneda <faontane@espol.edu.ec>
2021-12-01 00:10:55 -0500

    https://github.com/faontane-git/dawmparcial.git

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,6 +4,21 @@ export const SearchBar = () => {
 
     const text = document.getElementById("searchBarInput").value; //Variable que contiene el texto puesto en la barra de busqueda
     //Realizar aqui las operaciones necesarias para buscar las noticias segun lo que se ingreso en la barra de busqueda
+    let elementos = document.getElementsByClassName("news-container");
+    if (text.length > 0) {
+      for (let elemento of elementos) {
+        if (elemento.innerText.includes(text)) {
+          elemento.classList.add("visible");
+        } else {
+          elemento.classList.add("hidden");
+        }
+      }
+    } else {
+      for (let elemento of elementos) {
+        elemento.classList.remove("hidden");
+        elemento.classList.add("visible");
+      }
+    }
   };
 
   return (
