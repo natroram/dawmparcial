e8bea8e84d19d84a0e9de6d603e74a9b44d9071a
Fabrizzio Ontaneda <faontane@espol.edu.ec>
2021-12-01 23:54:02 -0500

    Modificación de los estílos de la sección noticias

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,8 +2,7 @@ export const SearchBar = () => {
   const onSearch = (e) => {
     e.preventDefault();
 
-    const text = document.getElementById("searchBarInput").value; //Variable que contiene el texto puesto en la barra de busqueda
-    //Realizar aqui las operaciones necesarias para buscar las noticias segun lo que se ingreso en la barra de busqueda
+    const text = document.getElementById("searchBarInput").value; 
     let elementos = document.getElementsByClassName("news-container");
     if (text.length > 0) {
       for (let elemento of elementos) {
@@ -24,14 +23,15 @@ export const SearchBar = () => {
   return (
     <form class="d-flex" id="searchBar" onSubmit={onSearch}>
       <input
-        class="form-control me-2"
+        class="form-control rounded"
         type="search"
         placeholder="Buscar"
         aria-label="Search"
         id="searchBarInput"
+        aria-describedby="search-addon"
       ></input>
-      <button class="btn btn-outline-success" type="submit">
-        Buscar
+      <button class="btn btn-outline-primary" type="submit">
+       &#x1f50d;&#xfe0e;
       </button>
     </form>
   );
