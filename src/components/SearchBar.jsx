8ac55e411e63b1c8673b3d8ec25a6a7461c9f189
Fabrizzio Ontaneda <faontane@espol.edu.ec>
2021-12-02 23:17:39 -0500

    Modicación de la vista Noticias pc y celular

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,11 +2,11 @@ export const SearchBar = () => {
   const onSearch = (e) => {
     e.preventDefault();
 
-    const text = document.getElementById("searchBarInput").value; 
+    const text = document.getElementById("searchBarInput").value.toLowerCase(); 
     let elementos = document.getElementsByClassName("news-container");
     if (text.length > 0) {
       for (let elemento of elementos) {
-        if (elemento.innerText.includes(text)) {
+         if (elemento.innerText.toLowerCase().includes(text)) {
           elemento.classList.add("visible");
         } else {
           elemento.classList.add("hidden");
